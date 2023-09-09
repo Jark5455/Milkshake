3d605d0670072582c4b1bc3b7d3a0ef84c28d7b9
yashr <rshailu.yash@gmail.com>
2023-09-09 19:12:23 -0400

    workflows prolly fixed

--- build.rs
+++ build.rs
@@ -1,15 +1,10 @@
 fn main() {
     let main_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("{}", main_dir);
 
-    let mut binding = std::process::Command::new("nvcc");
-    let command= binding
+    std::process::Command::new("nvcc")
         .arg("-ptx")
         .arg(format!("{}/cuda/loss.cu", main_dir))
         .arg("-o")
-        .arg(format!("{}/target/loss.ptx", main_dir));
-
-    println!("{:?}", command);
-
-    command.spawn().expect("Failed to compile kernel");
+        .arg(format!("{}/target/loss.ptx", main_dir))
+        .spawn().expect("Failed to compile kernel");
 }
