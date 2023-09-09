ee2fb36598214c275e13f45ee3137cf6a2ee6543
yashr <rshailu.yash@gmail.com>
2023-09-09 18:08:54 -0400

    test nvcc and build script with workflows

--- build.rs
+++ build.rs
@@ -2,10 +2,13 @@ fn main() {
     let main_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     println!("{}", main_dir);
 
-    std::process::Command::new("nvcc")
+    let command = std::process::Command::new("nvcc")
         .arg("-ptx")
         .arg(format!("{}/cuda/loss.cu", main_dir))
         .arg("-o")
-        .arg(format!("{}/target/loss.ptx", main_dir))
-        .spawn().expect("Failed to compile kernel");
+        .arg(format!("{}/target/loss.ptx", main_dir));
+
+    println!("{:?}", command);
+
+    command.spawn().expect("Failed to compile kernel");
 }
