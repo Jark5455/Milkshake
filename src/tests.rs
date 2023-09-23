72a09006b7c7cc434d57744e8b15593e16d8a047
yashr <rshailu.yash@gmail.com>
2023-09-23 11:35:24 -0400

    update tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -3,6 +3,8 @@ mod tests {
     use crate::stockenv::StockEnv;
     use dotenv::dotenv;
     use polars::export::chrono::{Duration, Utc};
+    use polars::prelude::FillNullStrategy;
+    use crate::stockframe::StockFrame;
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
@@ -18,5 +20,55 @@ mod tests {
         let start = end - Duration::days(15);
 
         let env = StockEnv::new(start, end);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_stockframe() {
+
+        dotenv().ok();
+
+        let end = Utc::now()
+            .date_naive()
+            .and_hms_micro_opt(0, 0, 0, 0)
+            .unwrap();
+
+        let start = end - Duration::days(15);
+
+        let mut stockframe = StockFrame::new(
+            Some(vec!["AAPL", "TLSA"].iter().map(|s| String::from(*s)).collect()),
+            Some(start.clone()),
+            Some(end.clone()),
+        );
+
+        stockframe.parse_dt_column();
+        stockframe.fill_date_range();
+        stockframe.fill_nulls();
+
+        unsafe {
+            stockframe.calc_technical_indicators();
+        }
+
+        // fill volume, vwap, and trade_count with zeros
+        stockframe.frame = Box::new(
+            stockframe
+                .clone()
+                .frame
+                .fill_null(FillNullStrategy::Zero)
+                .unwrap(),
+        );
+        stockframe.clean();
+
+        // sort
+        stockframe.update_symbol_groups();
+        stockframe.frame = Box::new(
+            stockframe
+                .clone()
+                .frame
+                .sort(&["symbol", "timestamp"], vec![false, false], false)
+                .unwrap(),
+        );
+
+        println!("{}", stockframe.frame);
     }
 }
