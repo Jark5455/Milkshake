6f2d66ed8adc4e4f35cc5a842a5b6ae18f331cfc
yashr <rshailu.yash@gmail.com>
2023-10-25 12:51:24 -0400

    optimized sub-optimal code

--- build.rs
+++ build.rs
@@ -1,3 +1,31 @@
+extern crate anyhow;
 extern crate cc;
-fn main() {
+extern crate glob;
+extern crate torch_build;
+
+fn main() -> anyhow::Result<()> {
+    let srcfiles = glob::glob("src/cpp/*.cpp")?.collect::<Result<Vec<std::path::PathBuf>, glob::GlobError>>()?;
+    let mut cargo_commands = vec![];
+
+    let mut build = cc::Build::new();
+
+    torch_build::build_cpp(
+        &mut build,
+        true,
+        false,
+        Some(&mut cargo_commands),
+        srcfiles.clone()
+    )?;
+
+    build.try_compile("pytorch-milkshake")?;
+
+    for file in srcfiles {
+        println!("cargo:rerun-if-changed={:?}", file.into_os_string().into_string().expect("Invalid UTF-8 Path"));
+    }
+
+    cargo_commands.iter().for_each(|command| {
+        println!("{}", command);
+    });
+
+    anyhow::Ok(())
 }
