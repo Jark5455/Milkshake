5ed3cc7c3ab153606360e181276fd684c5c39e62
yashr <rshailu.yash@gmail.com>
2023-11-08 19:54:26 -0500

    optimized sub-optimal code

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -4,5 +4,9 @@ pub trait MilkshakeOptimizer {
     fn new(xstart: &tch::Tensor) -> Box<dyn MilkshakeOptimizer> where Self: Sized;
     fn ask(&mut self);
     fn tell(&mut self, solutions: tch::Tensor, function_values: tch::Tensor);
-    fn stop(&mut self);
+    fn stop(&mut self) -> tch::Tensor;
+
+    fn disp(&mut self) {
+        println!("Display not implemented for this optimizer: {}", std::any::type_name::<Self>())
+    }
 }
